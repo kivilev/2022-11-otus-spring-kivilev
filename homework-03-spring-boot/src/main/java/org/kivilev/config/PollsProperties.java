c213a9ba1e232cf9237a8d3b2a55984630d0ce95
d.kivilev <d.kivilev@qiwi.com>
2022-12-19 08:55:55 +0300

    Fix problem with navigate from yaml file

--- homework-03-spring-boot/src/main/java/org/kivilev/config/PollsProperties.java
+++ homework-03-spring-boot/src/main/java/org/kivilev/config/PollsProperties.java
@@ -1,6 +1,5 @@
 package org.kivilev.config;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConstructorBinding;
@@ -9,9 +8,13 @@ import java.util.Locale;
 
 @ConfigurationProperties(prefix = "polls")
 @ConstructorBinding
-@AllArgsConstructor
 @Getter
 public class PollsProperties {
     private final int correctAnswersMinimumCount;
     private final Locale locale;
+
+    public PollsProperties(int correctAnswersMinimumCount, Locale locale) {
+        this.correctAnswersMinimumCount = correctAnswersMinimumCount;
+        this.locale = locale;
+    }
 }
