f131ab9ea4ed0aba82e3408208638dfcfe31bb12
d.kivilev <d.kivilev@qiwi.com>
2023-04-16 10:32:34 +0300

    Initial commit

---
+++ homework-17-docker/src/main/java/org/kivilev/rest/GenreRestController.java
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2023.
+ * Author: Kivilev Denis <kivilev.d@gmail.com>
+ */
+
+package org.kivilev.rest;
+
+import lombok.RequiredArgsConstructor;
+import org.kivilev.model.Genre;
+import org.kivilev.service.GenreService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+public class GenreRestController {
+    private final GenreService genreService;
+
+    @GetMapping("/api/v1/genres")
+    public List<Genre> listGenres() {
+        return genreService.getAllGenres();
+    }
+}
